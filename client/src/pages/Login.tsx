81ef33d9ae14a5b92b2dcc0aa70e662bda61d5f5
ajay <ajaypathak2527@gmail.com>
2024-05-08 23:03:13 +0530

    auth page ui changes

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Login(){
 
     async function login(e: React.FormEvent<HTMLFormElement>){
@@ -37,13 +39,13 @@ export default function Login(){
     }
 
     return(
-        <section className="flex flex-col justify-center items-center h-screen" >
-            <form onSubmit={login} className="max-w-md mx-auto flex flex-col gap-4 w-full p-8 border rounded-md shadow-lg" >
+        <section className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-600 to-violet-600 backdrop-blur-md" >
+            <form onSubmit={login} className="max-w-md mx-auto flex flex-col gap-4 w-full p-8 border rounded-lg shadow-lg bg-white" >
             <h2 className="text-2xl mb-8 font-semibold" >Login to continue</h2>
                 <input name="username" className="input input-bordered w-full" type="text" placeholder="Username" required={true} />
                 <input name="password" className="input input-bordered w-full" type="password" placeholder="Password" required={true} />
                 <button className="btn btn-primary w-full" >Login </button>
-                <p>Don't have an account? <a className="text-blue-500 hover:underline" href="/register" >Register</a> </p>
+                <p>Don't have an account? <Link className="text-blue-500 hover:underline" to="/register" >Register</Link> </p>
             </form>
         </section>
     )
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Register(){
 
     async function register(e: React.FormEvent<HTMLFormElement>){
@@ -35,15 +37,15 @@ export default function Register(){
     }
 
     return(
-        <section className="flex flex-col justify-center items-center h-screen" >
-            <form onSubmit={register} className="max-w-md mx-auto flex flex-col gap-4 w-full p-8 border rounded-md shadow-lg" >
+        <section className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-600 to-violet-600 backdrop-blur-md" >
+            <form onSubmit={register} className="max-w-md mx-auto flex flex-col gap-4 w-full p-8 border rounded-lg shadow-lg bg-white" >
                 <h2 className="text-2xl mb-8 font-semibold" >Create an account</h2>
                 <input name="username" className="input input-bordered w-full" type="text" placeholder="Username" required={true} />
                 <input name="password" className="input input-bordered w-full" type="password" placeholder="Password" required={true} />
                 <input name="confirmPassword" className="input input-bordered w-full" type="password" placeholder="Confirm Password" required={true} />
                 <button className="btn btn-primary w-full" >Login </button>
 
-                <p>Already have an account ? <a className="text-blue-500 hover:underline" href="/login" >Login</a> </p>
+                <p>Already have an account ? <Link className="text-blue-500 hover:underline" to="/login" >Login</Link> </p>
             </form>
         </section>
     )
