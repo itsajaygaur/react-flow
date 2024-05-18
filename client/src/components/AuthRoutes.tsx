3335935134301cef84d35a50e64baa9e52ede264
ajay <ajaypathak2527@gmail.com>
2024-05-18 16:19:55 +0530

    add loading state

--- client/src/components/AuthRoutes.tsx
+++ client/src/components/AuthRoutes.tsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import Loading from './Loading';
 
 export default function PrivateRoutes(){
 
     const { isLoggedIn, isLoading } = useAuth();
-    if(isLoading) return null
+    if(isLoading) return <Loading />  
 
     return(
         !isLoggedIn ? <Outlet/> : <Navigate to="/"/>
